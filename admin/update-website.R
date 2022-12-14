40d3c559889e54db19a8f4af5d8a8adc1c5a3421
seperry83 <seperry83@gmail.com>
2022-12-13 16:14:48 -0800

    2022 website update

---
+++ admin/update-website.R
@@ -0,0 +1,9 @@
+# run to compile html files for website
+# WARNING: after running, once pushed to GitHub, website will be updated
+
+bookdown::render_book(
+  input = 'admin/config-yaml.Rmd',
+  output_dir = './website-pages',
+  config_file = 'admin/_bookdown.yml',
+  output_format = 'bookdown::gitbook'
+)
