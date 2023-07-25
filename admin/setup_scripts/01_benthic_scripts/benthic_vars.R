2e4e1291c801dccd3a7ec29f5ba3d1de3da93c2c
Sarah Perry <seperry83@gmail.comv>
2023-07-25 10:46:36 -0700

    organize benthic

--- admin/setup_scripts/01_benthic_scripts/benthic_vars.R
+++
@@ -1,4 +0,0 @@
-# download data from EDI (assumption is all data will be downloaded for all; not great, but for now)
-df_raw <- get_edi_file(1036, glue::glue('DWR Benthic CPUE data 1975-{report_year}'))
-df_rawcount <- df_raw[[1]]
-df_cpue <- df_raw[[1]]
