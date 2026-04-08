2a2228fbea44ca506a1f478679c7695b7aff7d43
Perry <seperry83@gmail.com>
2026-04-07 17:35:01 -0700

    no fake data

--- admin/setup_scripts/03_other_scripts/mussel-data-prep.R
+++ admin/setup_scripts/03_other_scripts/mussel-data-prep.R
@@ -1,12 +1,12 @@
 
-library(dplyr)
-library(tidyr)
-library(readr)
-library(here)
-library(lubridate)
-library(jsonlite)
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(here)
+  library(lubridate)
+  library(jsonlite)
+})
 
-df_stations <- read_csv(here('admin/figures-tables/special-studies/Mussel_Station_Metadata.csv'))
+df_stations <- read_csv(here('admin/figures-tables/special-studies/Mussel_Station_Metadata.csv'), show_col_types = FALSE)
 sightings <- fromJSON(readLines(here('admin/test-data/mussel_data.json')))
 
 sightings <- sightings %>%
