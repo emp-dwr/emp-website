8ca13644610a212d64132c9cfb6567a2945f7ef7
Perry <seperry83@gmail.com>
2026-04-08 10:36:22 -0700

    suppressed code output

--- admin/setup_scripts/03_other_scripts/mussel-data-prep.R
+++ admin/setup_scripts/03_other_scripts/mussel-data-prep.R
@@ -1,12 +1,12 @@
 
-library(dplyr)
+suppressMessages({library(dplyr)
 library(tidyr)
 library(readr)
 library(here)
 library(lubridate)
-library(jsonlite)
+library(jsonlite)})
 
-df_stations <- read_csv(here('admin/figures-tables/special-studies/Mussel_Station_Metadata.csv'))
+df_stations <- read_csv(here('admin/figures-tables/special-studies/Mussel_Station_Metadata.csv'), show_col_types = FALSE)
 sightings <- fromJSON(readLines(here('admin/test-data/mussel_data.json')))
 
 sightings <- sightings %>%
